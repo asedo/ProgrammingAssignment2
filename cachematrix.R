f3254636824ecec489910e26d8b991d27e307be4
asedo <sapartgit@outlook.com>
2015-09-20 23:46:23 -0400

    Made a few modifications

--- cachematrix.R
+++ cachematrix.R
@@ -53,33 +53,6 @@ cacheSolve <- function(x, ...) {
 
 }
 
-makeVector <- function(x = numeric()) {
-        		m <- NULL
-	        set <- function(y) {
-		                x <<- y
-			        m <<- NULL
-			        }
-        			get <- function() x
-        			setmean <- function(mean) m <<- mean
-        			getmean <- function() m
-        			list(set = set, get = get,
-        			setmean = setmean,
-       				getmean = getmean)
-
-  					}
-
-cachemean <- function(x, ...) {
-        		m <- x$getmean()
-	       		 if(!is.null(m)) {
-		             message("getting cached data")
-		                return(m)
-				        }
-		        data <- x$get()
-		        m <- mean(data, ...)
-		        x$setmean(m)
-		        m
-			}
-
 
 #here are some Values you can use to run the program.
 #> x = rbind(c(1, 2), c(2, 1))
